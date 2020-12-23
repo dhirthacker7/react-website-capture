5cc6da44f913742dffb0a084314b5fe2f63c03cd
dhir-github <dhirthacker7@gmail.com>
2020-12-23 12:46:29 +0530

    6. Styling Services Section

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -2,6 +2,12 @@ import React from 'react';
 import home1 from '../img/home1.png';
 // styled
 import styled from 'styled-components';
+import {
+  StyledAbout,
+  StyledDescription,
+  StyledImage,
+  StyledHide
+} from '../styles';
 
 const AboutSection = () => {
   return (
@@ -32,38 +38,5 @@ const AboutSection = () => {
     </StyledAbout>
   )
 };
-
-// Styled components
-
-const StyledAbout = styled.div`
-  min-height: 90vh;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 5rem 10rem;
-  color: white;
-`
-
-const StyledDescription = styled.div`
-  flex: 1;
-  padding-right: 5rem;
-  h2 {
-    font-weight: lighter;
-  }
-`
-
-const StyledImage = styled.div`
-  flex: 1;
-  overflow: hidden;
-  img {
-    width: 100%;
-    height: 80vh;
-    object-fit: cover;
-  }
-`
-
-const StyledHide = styled.div`
-  overflow: hidden;
-`
 
 export default AboutSection;
